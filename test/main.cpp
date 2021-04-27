fd6c50fe2c44396cf5674fd84faa2b5f94ecf169
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-04-27 20:30:01 +0300

    add test and some update

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by focus on 27.04.2021.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+//
+// Created by focus on 27.04.2021.
+//
+
+#include "gtest/gtest.h"
+//#include <protocol.h>
+#include "protocol/protocol.h"
+
+
+TEST(CoreTest, ExampleTest1) {
+    ASSERT_TRUE(true);
+}
+
+TEST(CoreTest, ExampleTest2) {
+//    const auto expected = "Simple message from someLibDir";
+//    const auto actual = someLib::getMessage();
+//    EXPECT_STREQ(actual.c_str(), expected);
+}

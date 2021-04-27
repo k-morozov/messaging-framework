e92ff13303f80247df1088161e26c63fe6475352
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-04-27 08:49:11 +0300

    add cmake build

--- cmake/generate_proto_files.cmake
+++ cmake/generate_proto_files.cmake
@@ -8,20 +8,16 @@ function(generate_proto_files)
     # GENERATE PROTOBUF FILES
     file(GLOB PROTOBUF_FILES "include/protocol/proto/*.proto")
 
+
     find_package(Protobuf REQUIRED)
     protobuf_generate_cpp(PROTO_SRCS PROTO_HDRS ${PROTOBUF_FILES})
 
     # copy to ${CMAKE_BINARY_DIR}/proto
-    add_custom_target(protobuf_cpp_src_files ALL DEPENDS ${PROTO_SRCS})
-    add_custom_command(TARGET protobuf_cpp_src_files POST_BUILD
+    set(PROTO_FILES ${PROTO_SRCS} ${PROTO_HDRS})
+    add_custom_target(protobuf_cpp_src_files ALL DEPENDS ${PROTO_FILES})
+    add_custom_command(TARGET protobuf_cpp_src_files PRE_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            ${PROTO_SRCS} ${CMAKE_BINARY_DIR}/proto
-            )
-
-    add_custom_target(protobuf_cpp_hdr_files ALL DEPENDS ${PROTO_HDRS})
-    add_custom_command(TARGET protobuf_cpp_hdr_files POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            ${PROTO_HDRS} ${CMAKE_BINARY_DIR}/proto
+            ${PROTO_FILES} ${CMAKE_BINARY_DIR}/proto
             )
 
 endfunction()
